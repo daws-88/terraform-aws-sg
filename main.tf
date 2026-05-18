7eccf3120fd5b5ff59b8dffde41b5dbd8301f0a0
Venkat <psai96526@gmail.com>
2026-05-18 10:51:13 +0530

    imp

--- main.tf
+++ main.tf
@@ -2,6 +2,12 @@ resource "aws_security_group" "main" {
   name = var.sg_name # mongodb
   description = var.sg_description
   vpc_id = var.vpc_id
+   egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
   tags = merge(
     var.sg_tags,
     local.common_tags,
@@ -9,10 +15,4 @@ resource "aws_security_group" "main" {
       Name = "${local.common_name_suufix}-${var.sg_name}" 
     }
   )
-   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
 }
